86ec6329af62a1d564a456d6011b4705048e5755
Ville-Markus Aho <villemarkus.aho@edu.turkuamk.fi>
2022-09-10 13:16:40 +0300

    Restoring code to original state

--- lib/GUI/toolbar.py
+++ lib/GUI/toolbar.py
@@ -1,138 +1,151 @@
-import mplcursors  # pip install git+https://github.com/anntzer/mplcursors
-import pandas as pd  # pip install pandas
-# import matplotlib.pyplot as plt
+import mplcursors  # 'pip install git+https://github.com/anntzer/mplcursors'.
+import pandas as pd  # 'pip install pandas'
 from matplotlib.backend_tools import ToolToggleBase
-# from matplotlib.widgets import Button as mpl_button
 from lib.GUI.readfile import *
 
-
-# Custom toolbar button
+# Custom toolbar button for reading excel files.
 class OpenFileButton(ToolToggleBase):
     description = 'Choose excel file to read'
-
-    def __init__(self, *args, figure, plot, toolbar, **kwargs):
+    
+    def __init__(self, *args, figure, diagram, toolbar, **kwargs):
         self.toolbar = toolbar
         self.fig = figure
-        self.plot = plot
+        self.diagram = diagram
         super().__init__(*args, **kwargs)
-
+    
     def trigger(self, *args, **kwargs):
-        open_file(self.fig, self.plot, self.toolbar)
+        # Executed when OpenFileButton is pressed.
+        # Used to draw lines and configure charts.
+        draw_plot(self.fig, self.diagram, self.toolbar)
 
 
-def open_file(fig, plot, toolbar):
+def draw_plot(fig, diagram, toolbar):
     url = filepath()
-
     if url != '':
-        sheets = get_sheets(url)
+        # clear axis.
+        diagram.clear()
 
-        # if len(sheets) == 1:
-        draw_plot(fig, plot, toolbar, url, sheets[0])
-        # else:
-        # sheet = select_sheet(sheets)
-        # draw_plot(fig, plot, toolbar, url, sheet)
+        # Converts excel file into dataframe.
+        # openpyxl engine is needed for reading.
+        # .xlsx excel files
+        df = pd.read_excel(url, engine='openpyxl')
 
+        # Finds the columns where each value is null
+        # (=empty columns).
+        empty_cols = [col for col in df.columns if df[col].isnull().all()]
 
-# Executed when OpenFileButton is pressed
-# Used to draw lines and configure charts
-def draw_plot(fig, plot, toolbar, url, sheet):
-    # clear axis
-    plot.clear()
+        # Drops the columns where value is null.
+        df.drop(empty_cols, axis=1, inplace=True)
 
-    # Converts excel file into dataframe
-    df = pd.read_excel(url, sheet_name=sheet, engine='openpyxl')  # openpyxl engine is needed for reading
-    # .xlsx excel files
-    empty_cols = [col for col in df.columns if df[col].isnull().all()]  # Find the columns where each value is null (
-    # empty columns)
-    df.drop(empty_cols, axis=1, inplace=True)  # Drops the columns where value is null
+        # Sets file name as a chart title.
+        title = os.path.basename(url)
+        diagram.set_title(os.path.splitext(title)[0])
 
-    # Sets file name as a chart title
-    title = os.path.basename(url)
-    plot.set_title(os.path.splitext(title)[0])
+        # Reads first and second cells on the first line
+        # of the dataframe.
+        # x-axis title.
+        diagram.set_xlabel(df.iloc[:, 0].name)
 
-    # Reads first and second cells on the first line of the dataframe
-    plot.set_xlabel(df.iloc[:, 0].name)  # x-axis title
-    plot.set_ylabel(df.iloc[:, 1].name)  # y-axis title
+        # y-axis title.
+        diagram.set_ylabel(df.iloc[:, 1].name)
 
-    count = 1  # Tracks the plot line count
-    lines = []
+        count = 1  # Tracks the plot line count
+        lines = []
 
-    # Converts every row in each column into array
-    try:
-        for i in range(0, len(df.columns), 2):
-            x = df.iloc[:, i]
-            y = df.iloc[:, i + 1]
+        # Converts every row in each column into array
+        try:
+            for i in range(0, len(df.columns), 2):
+                x = df.iloc[:, i]
+                y = df.iloc[:, i + 1]
 
-            line, = plot.plot(x, y, label=f'Line {count}')
-            lines.append(line)
-            count += 1
+                line, = diagram.plot(x, y, label=f'Line {count}')
+                lines.append(line)
+                count += 1
 
-    except IndexError:
-        error_message('Index out of range ERROR', 'Excel taulukosta puuttuu sarake')
+        except IndexError:
+            error_message('Index out of range ERROR',
+            'Excel taulukosta puuttuu sarake')
+            return
+
+        except ValueError:
+            error_message('Invalid value ERROR',
+            'Sarakkeiden rivit sisältävät huonoja arvoja')
         return
-    except ValueError:
-        error_message('Invalid value ERROR', 'Sarakkeiden rivit sisältävät huonoja arvoja')
-        return
-
+ 
     # Legend info configurations
-    leg = plot.legend(bbox_to_anchor=(1.12, 1), loc='upper right', fancybox=True, shadow=True, title='Click to '
-                                                                                                     'toggle\n   line'
-                                                                                                     ' on/off\n')
-
+    leg = diagram.legend(bbox_to_anchor=(1.12, 1), loc='upper right', fancybox=True, shadow=True, title='Click to toggle\n line on/off\n')
     lined = dict()
+
     for legline, origline in zip(leg.get_lines(), lines):
-        legline.set_picker(5)  # 5 pts tolerance
+        legline.set_picker(5) # 5 pts tolerance
         lined[legline] = origline
 
-    # Event triggered when clicking legend lines
+    # Event triggered when clicking legend lines.
     def onpick(event):
-        # on the pick event, find the original line corresponding to the
-        # legend proxy line, and toggle the visibility
+        # on the pick event, find the original line
+        # corresponding to the legend proxy line,
+        # and toggle the visibility.
         try:
             legline = event.artist
             origline = lined[legline]
             vis = not origline.get_visible()
             origline.set_visible(vis)
-            # Change the alpha on the line in the legend so we can see what lines
-            # have been toggled
+
+            # Change the alpha on the line in the
+            # legend so we can see what lines have
+            # been toggled.
             if vis:
                 legline.set_alpha(1.0)
             else:
                 legline.set_alpha(0.2)
-            fig.canvas.draw()
+                fig.canvas.draw()
         except KeyError:
             pass
-
+    
     fig.canvas.mpl_connect('pick_event', onpick)
 
-    # adds grid to the canvas
-    plot.grid()
+    # adds grid to the canvas.
+    diagram.grid()
 
-    # adds tooltip to the cursor
-    tooltip = mplcursors.cursor(plot, hover=2)  # shows information about artist when hovering the cursor over it
-    marker = mplcursors.cursor(plot, multiple=True)  # this tooltip is used for setting markers to the plot lines
-
+    # adds tooltip to the cursor.
+    # tooltip shows information about artist when
+    # hovering the cursor over it.
+    tooltip = mplcursors.cursor(diagram, hover=2)
+ 
+    # this tooltip is used for setting markers to the
+    # plot lines.
+    marker = mplcursors.cursor(diagram, multiple=True)
+ 
     # tooltip configurations
     @tooltip.connect("add")
     def _(sel):
-        sel.annotation.get_bbox_patch().set(fc="white", alpha=1)  # Sets background color of tooltips
-        # sel.annotation.arrow_patch.set(alpha=0)  # Makes tooltip arrow transparent
-        sel.annotation.set_text(  # Information showed in the tooltip
+        # Sets background color of tooltips.
+        sel.annotation.get_bbox_patch().set(fc="white", alpha=1)
+        # Information showed in the tooltip.
+        sel.annotation.set_text(
+        # shows values in 10 and 3 decimal
+        # accuracy.
+        f'{sel.artist.get_label()}\n'
+        f'{df.iloc[:, 1].name.strip()}:'
+        f' {sel.annotation.xy[1]:.10f}'
+        f'\n{df.iloc[:, 0].name.strip()}:'
+        f' {sel.annotation.xy[0]:.3f}')
 
-            # shows values in three decimal accuracy
-            f'{sel.artist.get_label()}\n{df.iloc[:, 1].name.strip()}: {sel.annotation.xy[1]:.10f}'
-            f'\n{df.iloc[:, 0].name.strip()}: {sel.annotation.xy[0]:.3f}')
-
-    # second tooltip configurations
+    # second tooltip for tagging points of the plot.
     @marker.connect("add")
     def _(sel):
         sel.annotation.get_bbox_patch().set(fc="white", alpha=1)
         sel.annotation.set_text(
-            f'{sel.artist.get_label()}\n{df.iloc[:, 1].name.strip()}: {sel.annotation.xy[1]:.10f}\n'
-            f'{df.iloc[:, 0].name.strip()}: {sel.annotation.xy[0]:.3f}')
+        f'{sel.artist.get_label()}\n'
+        f'{df.iloc[:, 1].name.strip()}:'
+        f' {sel.annotation.xy[1]:.10f}\n'
+        f'{df.iloc[:, 0].name.strip()}:'
+        f' {sel.annotation.xy[0]:.3f}')
 
     fig.canvas.draw()
 
+    # functions below is for resetting view
+    # positions memory stack
     toolbar.update()
     tm = fig.canvas.manager.toolmanager
     reset = tm.get_tool('viewpos')
@@ -140,5 +153,3 @@ def draw_plot(fig, plot, toolbar, url, sheet):
     reset.add_figure(fig)
     reset.push_current()
     reset.update_view()
-
-    return True
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-from lib.GUI.toolbar import OpenFileButton
 import matplotlib as mpl  # pip install matplotlib==3.5.1
 import matplotlib.pyplot as plt
+from lib.GUI.toolbar import OpenFileButton
+import os
 # Also, install openpyxl package with 'pip install openpyxl'.
 
 mpl.rcParams["toolbar"] = 'toolmanager'
@@ -11,30 +11,36 @@ print(mpl.__version__)
 
 fig, ax = plt.subplots()
 
-# chart configurations
-ax.grid()
+# chart configurations.
 
-# window configurations
+ax.grid()
+# window configurations.
 manager = plt.get_current_fig_manager()
 manager.window.showMaximized()
 manager.set_window_title('Visualization tool')
 
-# adds custom button to the toolbar
+# adds custom button to the toolbar.
 tm = fig.canvas.manager.toolmanager
-tm.add_tool('openfile', OpenFileButton, plot=ax, figure=fig, toolbar=manager.canvas.manager.toolbar)
-button = tm.get_tool('openfile')
-# button.image = str(Path('assets/excel-icon.svg').absolute())  # Sets icon to the button
-button.image = "{0}/assets/excel-icon.svg".format(os.getcwd())
-manager.canvas.manager.toolbar.add_tool(button, 'toolgroup')  # adds button to the toolbar
+toolbar = manager.canvas.manager.toolbar
+tm.add_tool('openfile', OpenFileButton, diagram=ax,
+            figure=fig, toolbar=toolbar)
 
-# removes unwanted navigation toolbar buttons
+button = tm.get_tool('openfile')
+# Sets icon to the button.
+button.image = "{0}/assets/excel-icon.svg".format(os.getcwd())
+# adds button to the toolbar.
+manager.canvas.manager.toolbar.add_tool(button, 'toolgroup')
+
+# All toolbar default buttons are listed below, uncomment
+# or add "#" without quote marks if you want to remove or
+# add specific button.
 tm.remove_tool('subplots')
 tm.remove_tool('help')
 # tm.remove_tool('back')
 # tm.remove_tool('forward')
 # tm.remove_tool('home')
 
-# shortcut configurations
+# shortcut configurations.
 tm.update_keymap('zoom', 'x')
 
-plt.show()  # Display all open figures
+plt.show()  # Display all open figures.
